8a338110532895fbdb9088acb76feeb393f3394d
autumn-ragland <raglanda998@gmail.com>
2019-09-12 14:17:53 -0500

    Graded 09-12-2019 14:17:42

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// let check = document.getElementById('chckbox');
-//
-// let string = document.getElementById('dont');
+let check = document.getElementById('chckbox');
+
+let string = document.getElementById('dont'); // !! : this is a bad variable name 
 
 let type = document.getElementById('input');
 
@@ -8,14 +8,13 @@ let btton = document.getElementById('btton');
 
 // code for number 1
 // check.addEventListener('click', function (e) {
-//
+
 //     alert('I told you not to click');
 //     e.preventDefault();
-
-
 // });
 
 // code for number 2
+// !! : you should toggle this behaviour 
 btton.addEventListener('click',function (e) {
     type.removeAttribute('disabled');
     type.innerHTML = 'Type Something';
